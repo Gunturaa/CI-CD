74a2c768b6054d91a46918664e971079af02de53
Genosss <gunturhanabi222@gmail.com>
2025-05-12 19:41:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-def tambah(a, b):
-    return a + b
-
-def greet(nama):
-    return f"Hello, {nama}! Welcome to CI/CD."
-
-if __name__ == "__main__":
-    hasil = tambah(5, 3)
-    sapaan = greet("Guntur")
-
-    print(f"Hasil tambah: {hasil}")
-    print(sapaan)
+print("Hello From Developer A and B")
