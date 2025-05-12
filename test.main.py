7735f0d41a97fdf8b786714b0aab098f600111d3
Genosss <gunturhanabi222@gmail.com>
2025-05-12 19:33:03 +0700

    Update test.main.py

--- test.main.py
+++ test.main.py
@@ -4,6 +4,7 @@ def test_tambah():
     assert tambah(2, 3) == 5
     assert tambah(-1, 1) == 0
     assert tambah(0, 0) == 0
+    assert tambah(10, 5) == 15
 
 def test_greet():
     assert greet("Guntur") == "Hello, Guntur! Welcome to CI/CD."
