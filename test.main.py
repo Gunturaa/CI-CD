75e07decea7c5332954770638045f7160b50ae8f
Genosss <gunturhanabi222@gmail.com>
2025-05-12 19:40:56 +0700

    Update test.main.py

--- test.main.py
+++ test.main.py
@@ -1,1 +1,11 @@
-print("Hello From Developer A and B")
+from main import tambah, greet
+
+def test_tambah():
+    assert tambah(2, 3) == 5
+    assert tambah(-1, 1) == 0
+    assert tambah(0, 0) == 0
+    assert tambah(10, 5) == 15
+
+def test_greet():
+    assert greet("Guntur") == "Hello, Guntur! Welcome to CI/CD."
+    assert greet("World") == "Hello, World! Welcome to CI/CD."
